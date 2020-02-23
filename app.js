83d1d134aa017bf51a1a82f83ef0533eaa9be029
Olukayode <kay4sure_215@yahoo.com>
2020-02-23 17:55:54 +0100

    Third commit
    Added "author" to the blog

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const homeStartingContent = "<p>Lacus vel facilisis <em><strong>volutpat est velit egestas dui id ornare</strong></em>. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.</p>";
+const startHead = "<h1>My First Blog Site</h1>";
 const aboutContent = "<p>Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>";
 const contactContent = "<p>Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.</p>";
 
@@ -22,7 +23,8 @@ const blog = [];
 
 
 app.get("/", function (req, res) {
-  res.render("home", {homestart: homeStartingContent, x: blog});
+  dateAdded = date.getDate().toString()
+  res.render("home", {homestart: homeStartingContent, startHead: startHead, x: blog, dateAdded: dateAdded});
 });
 
 app.get("/about", function (req, res) {
@@ -35,15 +37,6 @@ app.get("/compose", (req, res) => res.render("compose"));
 
 
 app.get("/post/:route/", function(req, res){
-  // var check = blog.some(x => x.title === req.params.route)
-  // if (check)
-  //   {
-  //   console.log("matched");
-  // }
-  // else {
-  //   console.log("not matched" + " " +req.params.route);
-  // }
-  // })
 
 const reqFile = _.toLower(req.params.route);
 
@@ -52,7 +45,7 @@ blog.forEach(function(x){
 
 if(reqFile === lowerTitle)
   {
-    res.render("post" , {title : x.title, content: x.body, added: x.dateAdded});
+    res.render("post" , {title : x.title, content: x.body, added: x.dateAdded, author: x.author});
 }
 })
 })
@@ -62,16 +55,12 @@ app.post("/compose", function (req, res) {
   let post = {
     title : req.body.postTitle,
     body : req.body.postBody,
-    dateAdded : date.getDate().toString()
+    dateAdded : date.getDate().toString(),
+    author : req.body.author
   }
   blog.push(post);
   res.redirect("/")
 })
-
-
-
-
-
 
 
 
