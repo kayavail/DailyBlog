18efaf06bb2ad8574ede33e5dcaed6273fcb911f
Olukayode OJO <47598075+kayavail@users.noreply.github.com>
2021-09-27 08:12:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
-const homeStartingContent = "<p>This is not the first blog post<em><strong>. This was hard-coded to avoid emptiness on the page.</strong></em>. To add a post, click on the <strong>+</strong> beside the contact us menu. Note that you won't be able to update or delete at this moment for personal reason. This site was made under 24hrs, but the update can take eternity, ofcourse. I will update the content every now and then so don't worry about the mess or mistakes you make trying to post. The rest is Lorem Ipsum Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.</p>";
+const homeStartingContent = "<p>This is not the first blog post<em><strong>. This was hard-coded to avoid emptiness on the page.</strong></em>. To add a post, click on the <strong>+</strong> beside the contact us menu. Note that you won't be able to update or delete at this moment for personal reason. This site was made under 24hrs, but the update can take eternity, ofcourse. I will update the content every now and then so don't worry about the mess or mistakes you make trying to post. The rest is Lorem IpsumLacus vel facilisis <em><strong>volutpat est velit egestas dui id ornare</strong></em>. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.</p>";
 const startHead = "<h1>My First Blog Site</h1>";
 const aboutContent = ` <p><ol>
 <li><a href="https://kayavail.github.io/kaydog/">HTML CSS Bootstrap Project</a></li>
@@ -35,7 +35,7 @@ app.get("/", function (req, res) {
 
 app.get("/about", function (req, res) {
   // res.render("about", {about: aboutContent});
-  res.render("about");
+  res.render("about")
 });
 
 app.get("/contact", (req, res) => res.render("contact", {contact: contactContent}));
@@ -47,27 +47,42 @@ app.get("/post/:route/", function(req, res){
 
 const reqFile = _.toLower(req.params.route);
 
-blog.forEach(function(x){
-  const lowerTitle = _.toLower(x.title);
+blog.map(function(x){
+  const lowerTitle = _.toLower(x.id);
+ (reqFile === lowerTitle) && res.render("post" , {title : x.title, content: x.body, added: x.dateAdded, author: x.author});
 
-if(reqFile === lowerTitle)
-  {
-    res.render("post" , {title : x.title, content: x.body, added: x.dateAdded, author: x.author});
-}
+// if(reqFile === lowerTitle)
+//   {
+//     res.render("post" , {title : x.title, content: x.body, added: x.dateAdded, author: x.author});
+// }
+
+// {
+//   console.log("matched");
+// }
+//  {
+//   console.log("not matched" + " " +lowerTitle);
+// }
+
 })
 })
 
 
 app.post("/compose", function (req, res) {
   let post = {
+    id : Math.random() * 10000000000000000,
     title : req.body.postTitle,
     body : req.body.postBody,
     dateAdded : date.getDate().toString(),
     author : req.body.author
   }
   blog.push(post);
-  res.redirect("/")
+  res.redirect("/");
 })
+
+
+
+
+
 
 
 
